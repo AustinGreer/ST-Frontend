bb107e739a0088b0290cf645aaa7225fe79c3e3e
Austin Greer <agreer.ag12@gmail.com>
2021-10-07 01:11:11 -0400

    landing is good enough for now.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import { Route, Switch } from 'react-router-dom';
 import Landing from './components/Landing';
+import Login from './components/Login';
+import './styles/app.scss';
 
 function App() {
   return (
     <div>
       <Switch>
-        <Route path="/" component={Landing} />
+        <Route path="/login" component={Login} />
+        <Route exact path="/" component={Landing} />
       </Switch>
     </div>
   );
--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Landing() {
     return (
-        <div>
-            Here is the landing page
+        <div className="landing-screen">
+            <div className="intro-wrapper">
+                <div className="intro-text">
+                    <h1>Sleep Better,<br /> Sleep Smarter</h1>
+                    <p>Not everyone needs 8 hours of sleep, but how do you know if you're someone lucky enough to need only 6? or an unlucky one that needs 10?! Enter Sleep Tracker.</p>
+                    <Link to="/login">Get Started Today</Link>
+                </div>
+                <img src="/assets/sleep_smart.png" alt="sleep-tracker-img" />
+            </div>
         </div>
     )
 }
