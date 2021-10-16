c08da627ffc0f53dc5a02ac122b0e006952fabf6
Austin Greer <agreer.ag12@gmail.com>
2021-10-16 11:37:34 -0400

    login form values and input change now being controlled with state

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Input from '@mui/material/Input';
 import Button from '@mui/material/Button';
 import { Link } from "react-router-dom";
@@ -6,26 +6,56 @@ import { Link } from "react-router-dom";
 
 
 function Login() {
+    const [loginFormValues, setLoginFormValues] = useState({
+        username: "",
+        password: ""
+    })
+    const [funMessage, setFunMessage] = useState("")
+
+    const inputChangeHandler = (e) => {
+        setLoginFormValues({
+            ...loginFormValues,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const submitHandler = (e) => {
+        e.preventDefault()
+        
+        setLoginFormValues({
+            username: "",
+            password: ""
+        })
+
+        setFunMessage("Damnit dude, now I have to fill out the form again")
+    }
+
     return (
         <div className="login-wrapper">
             <div className="container">
                 <h1>Welcome Back!</h1>
                 <p>With the help of Sleep Tracker, you can discover your ideal sleep schedule today.</p>
 
-                <form>
+                {funMessage && <p className="fun-message">{funMessage}</p>}
+
+                <form onSubmit={submitHandler}>
                     <Input
                         type="text"
                         name="username"
                         placeholder="username"
                         disableUnderline={true}
+                        value={loginFormValues.username}
+                        onChange={inputChangeHandler}
                     />
                     <Input
                         type="password"
                         name="password"
                         placeholder="password"
                         disableUnderline={true}
+                        value={loginFormValues.password}
+                        onChange={inputChangeHandler}
                     />
-                    <Button>Login</Button>
+                    <Button type="submit">Login</Button>
                 </form>
 
                 <p>
